be444a2d7ebadcbba9b094c6aa279586926a969d
ZealousCynic <ZealousCynic@gmail.com>
2020-04-16 12:59:57 +0200

    Cleaned up unnecessary usings.

--- HashWithSalt/Data/SqlCon.cs
+++ HashWithSalt/Data/SqlCon.cs
@@ -1,10 +1,6 @@
 ﻿using HashWithSalt.Data;
 using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HashWithSalt
 {
